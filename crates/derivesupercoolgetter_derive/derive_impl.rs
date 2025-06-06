71cd9baa249e8c1f9b7efd4bc2cab70fd6b53932
Yuki Hiragino <yuki@shiina.family>
2025-06-07 02:40:57 +0900

    cleaner

--- crates/derivesupercoolgetter_derive/derive_impl.rs
+++ crates/derivesupercoolgetter_derive/derive_impl.rs
@@ -1,8 +1,9 @@
 use convert_case::{Case::Pascal, Casing};
 use guard_macros::guard;
+use itertools::Itertools;
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::{Ident, LitStr, Type, spanned::Spanned};
+use syn::{LitStr, spanned::Spanned};
 
 pub fn derive_impl(input: TokenStream) -> TokenStream {
     guard! {
@@ -16,65 +17,27 @@ pub fn derive_impl(input: TokenStream) -> TokenStream {
     let enum_ref_name = format_ident!("{}Ref", struct_name);
     let enum_mut_name = format_ident!("{}Mut", struct_name);
 
-    struct FieldExt {
-        ident_pascal: Ident,
-        lit: LitStr,
-        ident: Ident,
-        ty: Type,
-    }
-    let fields = fields
-        .named
-        .clone()
-        .into_iter()
-        .map(|field| {
-            let ident = field.ident.as_ref().unwrap().clone();
-            FieldExt {
-                ident_pascal: format_ident!("{}", ident.to_string().to_case(Pascal)),
-                lit: LitStr::new(&ident.to_string(), field.ident.span()),
-                ident,
-                ty: field.ty.clone(),
-            }
-        })
-        .collect::<Box<[_]>>();
-
-    let (enum_ref_items, enum_mut_items) = (
-        fields.iter().map(
-            |FieldExt {
-                 ident_pascal, ty, ..
-             }| quote!( #ident_pascal(&'a #ty) ),
-        ),
-        fields.iter().map(
-            |FieldExt {
-                 ident_pascal, ty, ..
-             }| quote!( #ident_pascal(&'a mut #ty) ),
-        ),
-    );
-    let (get_match_arms, get_mut_match_arms) = (
-        fields.iter().map(
-            |FieldExt {
-                 ident_pascal,
-                 lit,
-                 ident,
-                 ..
-             }| quote!( #lit => ::core::option::Option::Some(#enum_ref_name::#ident_pascal(&self.#ident)) )
-        ),
-        fields.iter().map(
-            |FieldExt {
-                 ident_pascal,
-                 lit,
-                 ident,
-                 ..
-             }| quote!( #lit => ::core::option::Option::Some(#enum_mut_name::#ident_pascal(&mut self.#ident)) )
-        ),
-    );
-    let (get_as_match_arms, get_mut_as_match_arms) = (
-        fields.iter().map(|FieldExt { lit, ident, .. }| {
-            quote! ( #lit => (&self.#ident as &dyn ::core::any::Any).downcast_ref() )
-        }),
-        fields.iter().map(|FieldExt { lit, ident, .. }| {
-            quote! ( #lit => (&mut self.#ident as &mut dyn ::core::any::Any).downcast_mut() )
-        }),
-    );
+    let (
+        enum_ref_items,
+        enum_mut_items,
+        get_match_arms,
+        get_mut_match_arms,
+        get_as_match_arms,
+        get_mut_as_match_arms,
+    ): (Vec<_>, Vec<_>, Vec<_>, Vec<_>, Vec<_>, Vec<_>) = fields.named.into_iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap().clone();
+        let ident_pascal = format_ident!("{}", ident.to_string().to_case(Pascal));
+        let lit = LitStr::new(&ident.to_string(), field.ident.span());
+        let ty = field.ty.clone();
+        (
+            quote!( #ident_pascal(&'a #ty) ),
+            quote!( #ident_pascal(&'a mut #ty) ),
+            quote!( #lit => ::core::option::Option::Some(#enum_ref_name::#ident_pascal(&self.#ident)) ),
+            quote!( #lit => ::core::option::Option::Some(#enum_mut_name::#ident_pascal(&mut self.#ident)) ),
+            quote!( #lit => (&self.#ident as &dyn ::core::any::Any).downcast_ref() ),
+            quote!( #lit => (&mut self.#ident as &mut dyn ::core::any::Any).downcast_mut() ),
+        )
+    }).multiunzip();
 
     quote! {
         pub enum #enum_ref_name<'a> {
@@ -115,4 +78,5 @@ pub fn derive_impl(input: TokenStream) -> TokenStream {
             }
         }
     }
+    .into()
 }
